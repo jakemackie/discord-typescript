acafb1d78f3458e681b3586fa8dcd058bdfd35ed
jakemackie <jake.wmackie@gmail.com>
2024-06-27 11:44:04 +0100

    Migration fixed

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import path from 'path';
+import '../../env';
 import postgres from 'postgres';
 
 const migrationClient = postgres(process.env.DATABASE_URL as string, {
